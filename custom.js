c5579b8ec746712f465cc71e843cac14aac8f353
zeeshanzaman <88842812+zeeshanzaman@users.noreply.github.com>
2022-10-10 23:22:49 +0500

    create else if condition

--- custom.js
+++ custom.js
@@ -6,6 +6,9 @@ codes.forEach((code, idx) => {
     code.addEventListener('keydown', (e) => {
         if(e.key >= 0 && e.key <=9 {
             setTimeout(() = codes[idx + 1].focus(),10)
-        })
+        } else if (e.key === 'Backspace') {
+            setTimeout(() = codes[idx - 1].focus(),10)
+        } 
+        )
     })
 })
